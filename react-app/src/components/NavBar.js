fc3935effc80514c4e0ad2953b14bde4f44fc0e2
Ayla Basha <aylabasha@gmail.com>
2021-11-18 00:23:19 -0800

    nav bar better

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,38 +2,121 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import { useSelector } from 'react-redux';
+
 import logo from '../images/haute-logo-bold.png'
 
 const NavBar = () => {
+  const sessionUser = useSelector(state => state.session.user);
+  if (sessionUser) {
+    return (
+      
+      <nav
+        style={{ backgroundColor: "#F0F0F0", height: "100px", display: "flex", position: 'right', listStyle: "none", padding: "0px" }}
+      >
+     
+          <li>
+            <NavLink to="/" exact={true} activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          {/* <li>
+            <NavLink to="/listings" exact={true} activeClassName="active">
+              Listings
+            </NavLink>
+          </li> */}
+          <li>
+            <LogoutButton />
+          </li>
+      
+      </nav>
+    );
+
+  } else {
+
   return (
     <>
-      <div className='nav-left'>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            <img className="HauteFinder-logo"src={logo} style={{position: 'absolute',marginLeft: '10px', height: '100px', maxHeight:'100%', maxWidth:'100%'}}></img>
-          </NavLink>
-        </div>
-      <nav style={{backgroundColor: '#F0F0F0', height: '110px', width:'100%', display: 'flex', margin: '0 auto'}}>
+      <div className="nav-left" style={{ width: "400px" }}>
+        <NavLink to="/" exact={true} activeClassName="active">
+          <img
+            className="HauteFinder-logo"
+            src={logo}
+            style={{
+              position: "absolute",
+              marginLeft: "10px",
+              height: "100px",
+              maxHeight: "100%",
+              maxWidth: "100%",
+            }}
+          ></img>
+        </NavLink>
+      </div>
+
+      <nav
+        style={{
+          display: "flex",
+          backgroundColor: "#F0F0F0",
+          height: "110px",
+          width: "100%",
+          display: "flex",
+          margin: "0 auto",
+          justifyContent: "flex-end",
+          decoration: "none",
+          listStyle: "none",
+        }}
+      >
         <li>
-          <NavLink to='/login' exact={true} activeClassName='active' style={{display: 'flex', color: '#763B2D', decoration: 'none', justifyContent:'flex-end'}}>
+          <NavLink
+            to="/login"
+            exact={true}
+            activeClassName="active"
+            style={{
+              display: "flex",
+              color: "#763B2D",
+              decoration: "none",
+              justifyContent: "flex-end",
+              padding: "10px",
+            }}
+          >
             Login
           </NavLink>
         </li>
         <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          <NavLink
+            to="/sign-up"
+            exact={true}
+            activeClassName="active"
+            style={{
+              display: "flex",
+              color: "#763B2D",
+              decoration: "none",
+              justifyContent: "flex-end",
+              padding: "10px",
+            }}
+          >
             Sign Up
           </NavLink>
         </li>
         <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          <NavLink
+            to="/users"
+            exact={true}
+            activeClassName="active"
+            style={{
+              display: "flex",
+              color: "#763B2D",
+              decoration: "none",
+              justifyContent: "flex-end",
+              padding: "10px",
+            }}
+          >
             Users
           </NavLink>
         </li>
-        <li>
-          <LogoutButton />
-        </li>
-    </nav>
-     </>
+      </nav>
+    </>
   );
+}
 }
 
 export default NavBar;
