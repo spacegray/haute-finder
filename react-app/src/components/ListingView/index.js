aa891434a2bf4735f70da0ebce09f7c0c8685abf
Ayla Basha <aylabasha@gmail.com>
2021-11-22 01:27:36 -0800

    styled

--- react-app/src/components/ListingView/index.js
+++ react-app/src/components/ListingView/index.js
@@ -49,7 +49,7 @@ function ListingView() {
 
   return (
     <div>
-      <h1>Listing View</h1>
+      <h1>{item?.name}</h1>
       <NewListingModal />
 
       <div className="create-listing-view">
