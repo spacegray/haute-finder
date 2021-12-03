45571f87ecbcd3995e888f9a54c3404dc1a69dda
Ayla Basha <aylabasha@gmail.com>
2021-12-02 17:30:47 -0800

    icons

--- react-app/src/components/Footer.js
+++ react-app/src/components/Footer.js
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./footer.css";
 
+
 const Footer = () => {
   return (
     <>
@@ -19,20 +20,23 @@ const Footer = () => {
           zIndex: "1",
         }}
       > */}
-        <div className="footer">
-            <div className="footer__iconSection">
-            <a
-                className="footer__icons"
-                href="https://www.linkedin.com/in/aylabasha/"
-            >
-                <i className="fab fa-linkedin"></i>
-            </a>
-            <a className="footer__icons" href="https://github.com/spacegray">
-                <i className="fab fa-github-square"></i>
-            </a>
-            </div>
-            <div className="footer__name"> Developed by Ayla Basha</div>
+      <div className="footer">
+        <div className="footer__iconSection">
+          <a
+            className="footer__icons"
+            href="https://www.linkedin.com/in/aylabasha/"
+          >
+            <i className="fab fa-linkedin"></i>
+          </a>
+          <a className="footer__icons" href="https://angel.co/u/ayla-basha">
+            <i class="fab fa-angellist"></i>
+          </a>
+          <a className="footer__icons" href="https://github.com/spacegray">
+            <i className="fab fa-github-square"></i>
+          </a>
         </div>
+        <div className="footer__name"> Developed by Ayla Basha</div>
+      </div>
       {/* </nav> */}
     </>
   );
