e0c2a3402aaefed33a17ddc6f06fee23dba5892f
Ayla Basha <aylabasha@gmail.com>
2022-02-15 15:12:45 -0800

    added borders and green hover btns. auth  pages

--- react-app/src/components/LoginPage/index.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import * as sessionActions from "../../store/session";
-import { useSelector, useDispatch } from "react-redux";
-import { Redirect } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { login } from "../../store/session";
-import logo from "../../images/spotifyLogo.png";
-import "./LoginPage.css";
-
